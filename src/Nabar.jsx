1c74eaba7d8b173df659431039b387b03abc705e
isnendyankp <isnendyankp@gmail.com>
2023-11-30 09:36:50 +0700

    import links, social

--- src/Nabar.jsx
+++ src/Nabar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { links, social } from './data'
 
 const Nabar = () => {
   return (
@@ -14,3 +15,4 @@ export default Nabar
 //  s7-244: create base Nabar component
 //  s7-244: import useState
 //  s7-244: import FaBars
+//  s7-244: import links, social
